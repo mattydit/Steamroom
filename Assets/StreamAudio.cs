e93233ae2c1b952ea9d53a6a75ccabe6b683cd73
Mateusz Harcej <c16344691@mydit.ie>
2020-04-06 01:27:31 +0100

    streaming stuff

---
+++ Assets/StreamAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Voice.Unity;
+
+public class StreamAudio : MonoBehaviour
+{
+    public Recorder recorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recorder = GetComponent<Recorder>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (recorder.IsRecording && recorder.AudioClip != null)
+        {
+            recorder.TransmitEnabled = true;
+        }
+    }
+}
